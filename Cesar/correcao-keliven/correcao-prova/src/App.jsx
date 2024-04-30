6ab1a6f1010b6278b662adfc0de2b8bfc7377bbe
CSR-Maroli <marolicesar@gmail.com>
2024-04-30 11:12:59 -0300

    correcao

--- Cesar/correcao-keliven/correcao-prova/src/App.jsx
+++ Cesar/correcao-keliven/correcao-prova/src/App.jsx
@@ -1,10 +1,14 @@
 import Evento from "./componentes/Evento"
 import './App.css'
 import Galeria from "./componentes/Galeria"
+import Cabecalho from "./componentes/Cabecalho"
+import Rodape from "./componentes/Rodape"
+
 function App() {
 
   return (
     <>
+    <Cabecalho />
      <Evento 
       titulo={"Senai"}
       descricao={"Venha fazer parte dessa era de gamers. Aqui no SENAI, para a nossa próxima gincana, tivemos a idéia de trazermos todas as nossas habilidades para campo. Traga o seu computador e venha participar de várias competições."}
@@ -12,6 +16,7 @@ function App() {
       local={"Na casa do melhor(João Pedro Benvenutti Vidal Cardoso)"}
      />
      <Galeria />
+      <Rodape />
     </>
   )
 }
