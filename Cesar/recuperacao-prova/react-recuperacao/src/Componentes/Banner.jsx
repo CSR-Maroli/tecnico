3c905b6ff71fe924abf15adc690aafcfb347b67f
César Maroli <marolicesar@gmail.com>
2024-05-06 05:26:34 -0300

    rec

---
+++ Cesar/recuperacao-prova/react-recuperacao/src/Componentes/Banner.jsx
@@ -0,0 +1,8 @@
+export default function Banner(){
+    return(
+        <section className="Banner">
+            <img src="https://www.maxieduca.com.br/blog/wp-content/uploads/2016/05/iStock_000083601441_Large-1024x728.jpg" alt="img de fundo"></img>
+            <h1 id="titulobanner">Biblioteca</h1>
+        </section>
+    )
+}
