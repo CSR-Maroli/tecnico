5958f1386a5caef8e8d04196385f183699d2bc68
CSR-Maroli <marolicesar@gmail.com>
2024-05-06 12:33:15 -0300

    prova

--- Cesar/recuperacao-prova/react-recuperacao/src/Componentes/Banner.jsx
+++ Cesar/recuperacao-prova/react-recuperacao/src/Componentes/Banner.jsx
@@ -1,7 +1,6 @@
 export default function Banner(){
     return(
         <section className="Banner">
-            <img src="https://www.maxieduca.com.br/blog/wp-content/uploads/2016/05/iStock_000083601441_Large-1024x728.jpg" alt="img de fundo"></img>
             <h1 id="titulobanner">Biblioteca</h1>
         </section>
     )
