43500b4e3fecd2acc1085c4234b9a2a079a37937
CSR-Maroli <marolicesar@gmail.com>
2024-08-05 12:33:47 -0300

    cgh

---
+++ Cesar/APP/teste/app/tabs/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text, Button, TextInput } from 'react-native';
+
+const App = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Calculadora</Text>
+      <TextInput></TextInput>
+    </View>
+  );
+};
+
+export default App;
