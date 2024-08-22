456f4379b23f754b9a7ee7ee7d784b7f770839c2
CSR-Maroli <marolicesar@gmail.com>
2024-08-22 11:04:22 -0300

    a

--- Cesar/APP/teste/app/tabs/index.jsx
+++ Cesar/APP/teste/app/tabs/index.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
-import { View, Text, Button, TextInput } from 'react-native';
+import { View, Image } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 const App = () => {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Calculadora</Text>
-      <TextInput></TextInput>
+    <LinearGradient
+        // Background Linear Gradient
+        colors={['rgba(0,0,0,0.8)', 'black']}
+        style={styles.background}
+      />
+      <Image
+        source={require('../tabs/img/Nike-Logo.png')}
+        style={{ width: 100, height: 100, }}
+        resizeMode="contain"
+      />
     </View>
   );
+
+
 };
+
 
 export default App;
